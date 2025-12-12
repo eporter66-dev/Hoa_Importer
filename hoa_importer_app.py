f23fd5bccd644fa14f3a9f25266e98cb4213aec6
rci <eporter@rotoloconsultants.com>
2025-12-12 16:01:28 -0600

    asdfasf

--- hoa_importer_app.py
+++ hoa_importer_app.py
@@ -587,10 +587,18 @@ if uploaded_file:
         st.warning("AAGO directory detected — fetching profile details using Selenium...")
 
         chrome_options = Options()
-        # chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+
+        chrome_options.add_argument(
+            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/122.0.0.0 Safari/537.36"
+        )
+
 
         driver = webdriver.Chrome(options=chrome_options)
 
